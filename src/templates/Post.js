89118e0ac42254722df21f2f484ae0b3e491c47a
Trevor Lang <trevorlang@ymail.com>
2020-03-15 18:28:42 -0600

    Fixing post issue

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
 
 import Layout from 'components/Layout/Layout';
 
@@ -11,21 +12,33 @@ const PortfolioPostPage = ({ data }) => {
   return (
     <Layout pageName="portfolio-post-page">
       <Helmet>
-        <title>{ title }</title>
+        <title>{title}</title>
       </Helmet>
-      <h1>{ title }</h1>
-      <p>{ date }</p>
+      <h1>{title}</h1>
+      <p>{date}</p>
       <div
         className="portfolio-post-page__content"
         dangerouslySetInnerHTML={{ __html: html }}
       />
     </Layout>
-    
   );
 };
 
 PortfolioPostPage.propTypes = {
   data: PropTypes.any
 };
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`;
 
 export default PortfolioPostPage;
